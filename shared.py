671a46c417517899f00ec0956afbc5100e7e5eec
Владислава Ли-Мин <vladaliming@gmail.com>
2026-06-05 11:45:09 +0300

    minor changes

--- shared.py
+++ shared.py
@@ -1,3 +1,23 @@
 cyr_alph = 'АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя'
 lat_alph = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz'
 special_char = 'ӘәҒғҚқҢңӨөҰұҮүҺһІі'
+
+# same as model.config.label2id
+label2id = {'ambig': 0,
+ 'kz': 1,
+ 'mixed_kz-ru': 2,
+ 'mixed_ru-kz': 3,
+ 'other': 4,
+ 'ru': 5,
+ 'skz': 6,
+ 'univ': 7}
+
+# same as model.config.id2label
+id2label = {0: 'ambig',
+ 1: 'kz',
+ 2: 'mixed_kz-ru',
+ 3: 'mixed_ru-kz',
+ 4: 'other',
+ 5: 'ru',
+ 6: 'skz',
+ 7: 'univ'}
